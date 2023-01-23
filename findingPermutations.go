bafcb14fedab310953bb152882f25c6df5f2f82d
Kyumamoon <kyumamoonsocial@gmail.com>
2023-01-23 00:21:55 -0800

    Added method to handle interfaces. Fixed findPermutations2 inaccuracy of handling length 2 slices.

--- findingPermutations.go
+++ findingPermutations.go
@@ -4,12 +4,10 @@ import "fmt"
 
 func findPermutation(start []int, depth int, permutations *([][]int)) {
 
-	permutationsStorage := [][]int{}
-
 	if depth == len(start) {
 		return
 	} else {
-
+		permutationsStorage := [][]int{}
 		for i := depth; i < len(start); i++ {
 			permutation := make([]int, len(start))
 			copy(permutation, start)
@@ -28,29 +26,57 @@ func findPermutation(start []int, depth int, permutations *([][]int)) {
 	}
 }
 
+func findPermutationInterface(start []interface{}, depth int, permutations *([][]interface{})) {
+
+	if depth == len(start) {
+		return
+	} else {
+		permutationsStorage := [][]interface{}{}
+		for i := depth; i < len(start); i++ {
+			permutation := make([]interface{}, len(start))
+			copy(permutation, start)
+			permutation[depth], permutation[i] = permutation[i], permutation[depth]
+
+			permutationsStorage = append(permutationsStorage, permutation)
+			if i != depth {
+				*permutations = append(*permutations, permutation)
+			}
+			//findPermutation(permutation, (depth + 1), permutations)
+		}
+
+		for _, e := range permutationsStorage {
+			findPermutationInterface(e, (depth + 1), permutations)
+		}
+	}
+}
+
 func findPermutation2(start []int, depth int, permutations *([][]int)) {
-	if depth == len(start)-1 {
+	if depth == len(start) {
 		return
 	} else {
 		for i := depth; i < len(start); i++ {
-			permutation := start
+			//permutation := start
+			permutation := make([]int, len(start))
+			copy(permutation, start)
 			permutation[depth], permutation[i] = permutation[i], permutation[depth]
 			if i != depth {
 				*permutations = append(*permutations, permutation)
 			}
-			findPermutation(permutation, (depth + 1), permutations)
+			findPermutation2(permutation, (depth + 1), permutations)
 		}
 	}
 }
 
 func main() {
-	x := []int{1, 2, 3, 4, 5}
+	//x := []interface{}{1, "a", true}
+	x := []int{1}
 	answer := [][]int{x}
 	findPermutation2(x, 0, &answer)
-
-	fmt.Println("PERMUTATIONS:", len(answer))
 
 	for _, e := range answer {
 		fmt.Println(e)
 	}
+
+	fmt.Println("PERMUTATIONS:", len(answer))
+	//fmt.Println(answer)
 }
